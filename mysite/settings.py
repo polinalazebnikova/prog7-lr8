8f7e23f5e9c0ccdf73cf05ae75363f51220b2241
Polina Lazebnikova <54927872+polinalazebnikova@users.noreply.github.com>
2022-01-11 23:55:48 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pqi30p9u1_rmp=rgr@&17sx_pbs5qyo5doo+s90^kz^oifqt!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
